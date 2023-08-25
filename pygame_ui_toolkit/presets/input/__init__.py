aef60a50306d6a6fa9777a307e53ed6590e33cc5
Ben-Edwards44 <benjedwards007@icloud.com>
2023-08-25 11:08:07 +0100

    Changed folder name

--- pygame_ui_toolkit/presets/input/__init__.py
+++ pygame_ui_toolkit/presets/input/__init__.py
@@ -1,5 +1,5 @@
 from pygame_ui_toolkit.elements import input
-from pygame_ui_toolkit.presets.button import size_change
-from pygame_ui_toolkit.presets.button import colour_change
+from pygame_ui_toolkit.presets.buttons import size_change
+from pygame_ui_toolkit.presets.buttons import colour_change
 from pygame_ui_toolkit import utils
 import pygame
